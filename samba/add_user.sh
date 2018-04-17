ca3f4aebcdd0098f4281761fa367f8aaa3950d1a
Nicolas Berard <nicolas.berard@canaltp.fr>
2018-04-17 11:43:14 +0200

    commit

--- samba/add_user.sh
+++ samba/add_user.sh
@@ -15,21 +15,21 @@ while getopts ':u:p:h:i' flag; do
     *) echo "Unexpected option ${flag}" ;;
   esac
 done
-tee -a /etc/samba/smb.conf << EOF
-;start_config_$USERNAME
-[share_$USERNAME]
-    guest ok = no
-    comment = Samba share for user $USERNAME
-    path = /home/users/$USERNAME/Downloads
-    browsable = yes
-    read only = no
-    writeable = yes
-    locking = no
-    follow symlinks = yes
-    wide links = yes
-;end_config_$USERNAME
-EOF
+#tee -a /etc/samba/smb.conf << EOF
+#;start_config_$USERNAME
+#[share_$USERNAME]
+#    guest ok = no
+#    comment = Samba share for user $USERNAME
+#    path = /home/users/$USERNAME/Downloads
+#    browsable = yes
+#    read only = no
+#    writeable = yes
+#    locking = no
+#    follow symlinks = yes
+#    wide links = yes
+#;end_config_$USERNAME
+#EOF
 adduser -D -H $USERNAME
 (echo "$PASSWORD"; sleep 1; echo "$PASSWORD" ) | smbpasswd -s -a $USERNAME
-ln -s /home/users /home/users/$USERNAME/Downloads/global
+#ln -s /home/users /home/users/$USERNAME/Downloads/global
 exit 0
